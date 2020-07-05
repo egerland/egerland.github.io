b08e30213c650cb27294e056025e68eb7b6f0827
Pedro <egerlandpedro@gmail.com>
2020-07-05 00:18:31 -0300

    Updates

---
+++ precache-manifest.c0c05b6062f19c3533ced338476e7f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15e1aa195ec025f8a2bb47f683bc9c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2fd49c81e47ab9f6f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29d0b79bac9f3506e811",
+    "url": "/static/js/2.8512d60b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8512d60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2fd49c81e47ab9f6f4",
+    "url": "/static/js/main.384e8aab.chunk.js"
+  },
+  {
+    "revision": "8dc05a874a2ec2d083d4",
+    "url": "/static/js/runtime-main.83897c69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
